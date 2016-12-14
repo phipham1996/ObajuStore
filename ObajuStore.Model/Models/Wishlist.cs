b0115407c410d0a2ef0f382cbe66e7ee5d5afd2b
dvbtham <dvbtham@gmail.com>
2016-12-14 20:06:20 +0700

    Tạo các table cho cơ sở dữ liệu

---
+++ ObajuStore.Model/Models/Wishlist.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ObajuStore.Model.Abstracts;
+
+namespace ObajuStore.Model.Models
+{
+    [Table("Wishlists")]
+    public class Wishlist : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string UserId { get; set; }
+
+        public long ProductId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
